39acaffd419f0ccca83d7d190ad9bd0b32fbb615
Josue Lopez <galvez2j@gmail.com>
2020-12-01 13:19:32 -0800

    Create errors.py for custom exceptions

---
+++ src/errors.py
@@ -0,0 +1,8 @@
+# errors.py
+#
+# Specify custom error exceptions, standards, code mappings
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
