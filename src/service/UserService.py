1c252d586240031ae9709f2131cded018a190d93
mohra-arsala <marsala@uci.edu>
2020-12-02 02:16:58 -0800

    removed hardcoded userId for new users

--- src/service/UserService.py
+++ src/service/UserService.py
@@ -10,8 +10,8 @@ def addUser(params):
         user = User(params.get('user_name'), params.get('first_name'), params.get('last_name'), params.get('email'), params.get('phone_number'))
         print(user.__dict__)
         
-        sql = "INSERT INTO [User] (UserId, userName, firstName, lastName, email, phoneNumber, isValidated) \
-            VALUES(3, ?, ?, ?, ?, ?, 0)" # id is hardcoded, need to update db to auto increment id
+        sql = "INSERT INTO [User] (userName, firstName, lastName, email, phoneNumber, isValidated) \
+            VALUES(?, ?, ?, ?, ?, 0)"
         dbConn = getConnection();
         update = performDBUpdate(dbConn, sql, user.user_name, user.first_name, user.last_name, user.email, user.phone_number)
         return True # db update successful when the function reaches this point
